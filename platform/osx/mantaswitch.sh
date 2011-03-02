b8b08d7b466f020979f59f6cb25bcf9d0998ee1b
Spencer Russell <spencer.f.russell@gmail.com>
2011-03-01 23:24:19 -0500

    adds status output to mantaswitch script

--- platform/osx/mantaswitch.sh
+++ platform/osx/mantaswitch.sh
@@ -4,9 +4,10 @@ FILENAME=MantaDummyDriver.kext
 
 if  [ -d $DEST_DIR/$FILENAME ]
 then
-    rm -r $DEST_DIR/$FILENAME
+    rm -r $DEST_DIR/$FILENAME && echo $FILENAME Removed, please reboot
 else
-    cp -r $FILENAME $DEST_DIR
-    chmod -R 755 $DEST_DIR/$FILENAME
-    chown -R 0:0 $DEST_DIR/$FILENAME
+    cp -r $FILENAME $DEST_DIR && \
+    chmod -R 755 $DEST_DIR/$FILENAME && \
+    chown -R 0:0 $DEST_DIR/$FILENAME && \
+    echo $FILENAME Installed, please reboot
 fi
