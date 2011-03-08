668482e0dfb0897e993be39217925d88b537c0ac
Spencer Russell <spencer.f.russell@gmail.com>
2011-03-07 23:37:51 -0500

    adds init message for library, with props

--- MantaFlext/MantaFlextLibrary.cpp
+++ MantaFlext/MantaFlextLibrary.cpp
@@ -9,6 +9,11 @@ static void mantaLibSetup(void)
    FLEXT_SETUP(manta);
    FLEXT_SETUP(MantaFlextCentroid);
    FLEXT_SETUP(MantaFlextMaximum);
+   post("manta: PD / Max object by Spencer Russell");
+   post("manta: Centroid Algorithm by R. Luke Dubois");
+   post("manta: Velocity Algorithm by Angie Hugeback");
+   post("manta: Much thanks to Jeff Snyder at Snyderphonics");
+   post("manta: Built on Flext by Thomas Grill");
 }
 
 FLEXT_LIB_SETUP(manta, mantaLibSetup)
