e5d67b6e30cbddae6dcadcffb301d0d14fb8ee8b
Spencer Russell <spencer.f.russell@gmail.com>
2012-01-22 13:51:32 -0500

    adds version reporting

--- MantaFlext/MantaFlextLibrary.cpp
+++ MantaFlext/MantaFlextLibrary.cpp
@@ -1,6 +1,8 @@
 #include <flext.h>
 #include "MantaFlextCentroid.h"
 #include "MantaFlext.h"
+#include "MantaFlextVersion.h"
+#include "../core/MantaVersion.h"
 
 static void mantaLibSetup(void)
 {
@@ -11,6 +13,9 @@ static void mantaLibSetup(void)
    FLEXT_SETUP(MantaFlextMaximum);
    FLEXT_SETUP(MantaFlextPoly);
    post("manta: PD / Max object by Spencer Russell");
+   post("manta: MantaFlext V %02d.%02d, libmanta V %02d.%02d",
+         MANTAFLEXT_MAJOR_VERSION,MANTAFLEXT_MINOR_VERSION, 
+         LIBMANTA_MAJOR_VERSION,LIBMANTA_MINOR_VERSION);
    post("manta: Centroid Algorithm by R. Luke Dubois");
    post("manta: Velocity Algorithm by Angie Hugeback");
    post("manta: Much thanks to Jeff Snyder at Snyderphonics");
